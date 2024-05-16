aab4ec50c93b575b4d953301c5fc50329904d575
minhtran291 <minhtran2912003@gmail.com>
2024-05-16 08:12:44 +0700

    add file ConsoleApp1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Processing_Command_Line_Arguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String msg = "***** Welcome to C# .NET Programming *****";
+            Console.WriteLine("{0}", msg);
+            
+            for(int i = 0; i < args.Length; i++) {
+                Console.WriteLine($"Arg: {args[i]}");
+            }
+            //Console.ReadLine();
+        }
+    }
+}
+
